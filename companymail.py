25e94e08804f1aae40fbc0ddb7a60edcadb95d79
jabinhuewine032 <jabinhuewine032@gmail.com>
2026-06-07 18:36:51 +0530

    first time python

---
+++ companymail.py
@@ -0,0 +1,9 @@
+# welcome guys
+import sys
+full_name = sys.argv[1]
+
+email = full_name.lower().replace(".", ".")+ "@techmadibdha.com"
+
+print("---your profile---")
+print("full name:", full_name)
+print("Generated email:", email)
